eb5be9304aab8fdc425c7ae7ada69e1f31291b25
gizmore <gizmore@wechall.net>
2022-07-20 02:32:15 +0200

    '7.0.0-r1361'

--- tpl/form/position.php
+++ tpl/form/position.php
@@ -14,6 +14,6 @@ $field instanceof GDT_Position;
    <?=$field->htmlDisabled()?>
    size="12"
    <?=$field->htmlFormName()?>
-   value="<?= $field->display(); ?>" />
+   value="<?= $field->renderVar(); ?>" />
   <?= $field->htmlError(); ?>
 </div>
