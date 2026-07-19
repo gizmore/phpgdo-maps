a0a151348b9618a1c454212ba354b8df05745fce
gizmore <gizmore@wechall.net>
2026-07-19 15:17:09 +0200

    'ws'

--- js/gdo-maps-record.js
+++ js/gdo-maps-record.js
@@ -7,7 +7,7 @@ GDO.Maps = GDO.Maps || {};
 
 GDO.Maps.positioning = function() {
 	console.log('GDO.Maps.positioning()');
-	navigator.geolocation.getCurrentPosition(GDO.Maps.gotPosition, GDO.error);
+	navigator.geolocation.getCurrentPosition(GDO.Maps.gotPosition, GDO.Maps.positioningError);
 };
 
 GDO.Maps.gotPosition = function(pos) {
@@ -15,9 +15,16 @@ GDO.Maps.gotPosition = function(pos) {
 	let c = pos.coords;
 	window.GDO.gdoxhr('Maps', 'Record', '&_fmt=json&pos_lat='+c.latitude+'&pos_lng='+c.longitude).then(function(result) {
 		console.log('wrote position', result);
+		setTimeout(GDO.Maps.positioning, window.GDO_MAPS_HISTORY * 1000);
 	});
 };
 
+GDO.Maps.positioningError = function(error) {
+	console.log('GDO.Maps.positioningError()', error);
+	debugger;
+	return GDO.error(error.message, t('positioning')).then(() => setTimeout(GDO.Maps.positioning, window.GDO_MAPS_HISTORY * 1000));
+};
+
 if (navigator.geolocation) {
-    setInterval(GDO.Maps.positioning, window.GDO_MAPS_HISTORY * 1000);
+	setTimeout(GDO.Maps.positioning, window.GDO_MAPS_HISTORY * 1000);
 }
