14ead788f30a39a1045c75a268309eac5d109a75
gizmore <gizmore@wechall.net>
2026-09-18 04:04:52 +0200

    'more korean'

---
+++ lang/maps_ko.php
@@ -0,0 +1,21 @@
+<?php
+namespace GDO\Maps\lang;
+
+return [
+	'module_maps' => '지도',
+	'position' => '위치',
+	'latitude' => '위도',
+	'longitude' => '경도',
+	'velocity' => '속도(km/h)',
+	'cfg_max_velocity' => '속도 기록(km/h)',
+	'cfg_max_keypress' => '분당 활동기록 입력',
+	'info_div_maps_google' => 'Google 위치 코드 및 데이터의 상태입니다.',
+	'cfg_maps_api_google' => 'GoogleMaps(tm) 자바스크립트',
+	'cfg_maps_sensors' => '구글 포지셔닝',
+	'cfg_maps_record_history' => '위치 간격(0=끄기)',
+	'cfg_maps_record' => 'GDOv7 포지셔닝',
+	'info_div_maps_gdo' => '자신의 위치 코드 및 데이터 상태입니다.',
+	'mt_maps_record' => '기록',
+	'err_no_position' => '위치 데이터가 없습니다.',
+	'msg_location_recorded' => '귀하의 위치가 기록되었습니다.',
+];
